a702eda0dddd0a4341151bebc92ccfb9e1f52122
Romain Lebbadi-Breteau <romain@lebbadi.fr>
2026-04-27 14:37:34 -0400

    other changes

--- RUN-CSP-v2/setup.sh
+++ RUN-CSP-v2/setup.sh
@@ -1,2 +1,7 @@
+# pushd .venv/lib/python3.13/site-packages/tensorflow
+# ln -svf ../nvidia/*/lib/*.so* .
+# popd
+# ln -sf ../lib/python3.13/site-packages/nvidia/cuda_nvcc/bin/ptxas $VIRTUAL_ENV/bin/ptxas
+
 # source me
 export LD_LIBRARY_PATH=$PWD/.venv/lib/python3.13/site-packages/tensorflow
